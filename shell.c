c690cf83536021909b4f27d28c897e6132e4b110
Emilien-Marot <12394@holbertonstudents.com>
2026-01-08 11:06:50 +0000

    v0.0.7

--- shell.c
+++ shell.c
@@ -12,7 +12,7 @@ void l_free(char **list_w)
 {
 	int i;
 
-	for(i = 0; list_w[i] != NULL; i++)
+	for (i = 0; list_w[i] != NULL; i++)
 	{
 		free(list_w[i]);
 		list_w[i] = NULL;
@@ -24,24 +24,24 @@ int string_to_list(char *string, char **list)
 	int i, j = 0, idx_w = 0;
 	char *word;
 
-	if(list == NULL || string == NULL)
-		return(1);
+	if (list == NULL || string == NULL)
+		return (1);
 	word = malloc(BUF_SIZE * sizeof(char));
-	if(word == NULL)
-		return(1);
+	if (word == NULL)
+		return (1);
 	j = 0;
 	idx_w = 0;
 	word[0] = '\0';
-	for(i = 0; string[i] != '\0'; i++)
+	for (i = 0; string[i] != '\0'; i++)
 	{
 		if (string[i] == ' ' || string[i] == '\n')
 		{
 			list[idx_w] = strdup(word);
-			if(list[idx_w] == NULL)
+			if (list[idx_w] == NULL)
 			{
 				list[idx_w] = NULL;
 				l_free(list);
-				return(1);
+				return (1);
 			}
 			idx_w++;
 			list[idx_w] = NULL;
@@ -54,7 +54,7 @@ int string_to_list(char *string, char **list)
 		}
 		word[j] = '\0';
 	}
-	return(0);
+	return (0);
 }
 
 /**
@@ -65,67 +65,60 @@ int string_to_list(char *string, char **list)
 int main(void)
 {
 	pid_t child_pid;
-	int status, i = 0;
+	int status, i = 0, ret = 0;
 	size_t buf_size = BUF_SIZE;
-	char **list_w;
-	char *buffer;
+	char **list_w, *buffer;
 	ssize_t res;
 
 	buffer = malloc(BUF_SIZE * sizeof(char));
-	if(buffer == NULL)
+	if (buffer == NULL)
 	{
 		perror("Error: could not allocate memory to the buffer");
-		return(1);
+		return (1);
 	}
 	list_w = malloc(BUF_SIZE * sizeof(char *));
-	if(list_w == NULL)
+	if (list_w == NULL)
 	{
 		perror("Error: could not allocate memory to the buffer");
 		free(buffer);
-		return(1);
+		return (1);
 	}
-	do
-	{
+	do {
 		i++;
 		printf("$ ");
 		res = getline(&buffer, &buf_size, stdin);
 		if (res == -1)
 		{
-			if(errno != 0)
+			if (errno != 0)
 				perror("Error_1");
 			else
 				printf("\n");
 			break;
 		}
-		if(string_to_list(buffer, list_w) == 1)
+		if (string_to_list(buffer, list_w) == 1)
 		{
-			free(buffer);
-			l_free(list_w);
-			free(list_w);
 			perror("Error_2");
-			return(1);
+			ret = 1;
+			break;
 		}
 		child_pid = fork();
 		if (child_pid == -1)
 		{
-			free(buffer);
-			l_free(list_w);
-			free(list_w);
 			perror("Error_3");
-			return (1);
+			ret = 1;
+			break;
 		}
 		if (child_pid == 0)
 		{
 			if (execve(list_w[0], list_w, NULL) == -1)
-			perror("Error_4");
+				perror("Error_4");
 		}
 		else
 			wait(&status);
-	}
-	while (child_pid != 0);
-	free(buffer);
+	} while (child_pid != 0);
 	l_free(list_w);
 	free(list_w);
+	free(buffer);
 	return (0);
 }
 
