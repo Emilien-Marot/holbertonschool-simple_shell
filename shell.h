6fbe66b128b7aa340cf84e901811f771754482ec
Emilien-Marot <12394@holbertonstudents.com>
2026-01-10 19:33:04 +0000

    manual revert to 0.3.10

--- shell.h
+++ shell.h
@@ -4,22 +4,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
+#include<string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
 #include <linux/limits.h>
 
-extern char **environ;
-
-char *_getenv(const char *name);
-void print_env();
-void get_path_loop(char *name, char *paths, char *full);
-char *get_path(char *name);
 void l_free(char **list_words);
 int string_to_list(char *string, char **list);
-int prompt_init(char *buffer, char **list_words);
-int exec_prompt(const char *shell_name, char *buffer, char **list_words);
-int prompt(const char *shell_name);
+int prompt();
+int main(int ac __attribute__((unused)), char **av);
 
 #endif
