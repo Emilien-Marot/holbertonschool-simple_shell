8bb847084bb10d31c5b4ce86421438a726851f92
Emilien-Marot <12394@holbertonstudents.com>
2026-01-10 14:28:36 +0000

    betty check

--- shell.c
+++ shell.c
@@ -2,121 +2,9 @@
 
 #define BUF_SIZE 1024
 
-extern char **environ;
-
-/*TODO*/
-char *_getenv(const char *name)
-{
-	int i, j, same;
-
-	if(environ == NULL)
-		return(NULL);
-	for(i = 0; environ[i] != NULL; i++)
-	{
-		same = 1;
-		for(j = 0; name[j] != '\0'; j++)
-		{
-			if(environ[i][j] != name[j])
-			{
-				same = 0;
-				break;
-			}
-		}
-		if(same && environ[i][j] == '=' && name[j] == '\0')
-		{
-			return(strdup(&(environ[i][j+1])));
-		}
-	}
-	return(NULL);
-}
-
-void print_env()
-{
-	int i;
-
-	if(environ == NULL)
-		return;
-	for(i = 0; environ[i] != NULL; i++)
-	{
-		printf("%s\n", environ[i]);
-	}
-}
-
-/*TODO*/
-char *get_path(char *name)
-{
-	char *PATH, *path, *full;
-	int i, j;
-
-	if (name == NULL)
-		return(NULL);
-	if(access(name, F_OK) == 0)
-	{
-		return(strdup(name));
-	}
-	PATH = _getenv("PATH");
-	if(PATH == NULL)
-		return(NULL);
-	full = malloc(PATH_MAX * sizeof(char));
-	if(full == NULL)
-	{
-		free(PATH);
-		return(NULL);
-	}
-	path = strtok(PATH, ":");
-	while(path != NULL)
-	{
-		i = 0;
-		full[0] = '\0';
-		while(path[i] != '\0')
-		{
-			full[i] = path[i];
-			i++;
-			full[i] = '\0';
-		}
-		full[i] = '/';
-		i++;
-		full[i] = '\0';
-		j = 0;
-		while(name[j] != '\0')
-		{
-			full[i+j] = name[j];
-			j++;
-			full[i+j] = '\0';
-		}
-		if(access(full, F_OK) == 0)
-		{
-			free(PATH);
-			return(full);
-		}
-		path = strtok(NULL, ":");
-	}
-	free(PATH);
-	free(full);
-	return(NULL);
-}
-
-/**
- * l_free - free a list of strings and all the strings it points to
- * 
- * @list_words: list of strings to be freed
- *
- * Return: void
- */
-void l_free(char **list_words)
-{
-	int i = 0;
-
-	for (i = 0; list_words[i] != NULL; i++)
-	{
-		free(list_words[i]);
-		list_words[i] = NULL;
-	}
-}
-
 /**
  * string_to_list - take a string and separate it into a list of words
- * 
+ *
  * @string: string to be converted into a list
  * @list: list that will contain the strings
  *
@@ -124,7 +12,7 @@ void l_free(char **list_words)
  */
 int string_to_list(char *string, char **list)
 {
-    int i = 0;
+	int i = 0;
 
 	if (list == NULL)
 		return (-1);
@@ -136,43 +24,24 @@ int string_to_list(char *string, char **list)
 	list[0] = strtok(string, "\t\n ");
 	while (list[i] != NULL)
 	{
-	    i++;
-	    list[i] = strtok(NULL, "\t\n ");
+		i++;
+		list[i] = strtok(NULL, "\t\n ");
 	}
 	return (0);
 }
 
 /**
- * prompt - waits for a command from stdin and execute it in a child process
- * 
- * @buffer: buffer that will contain the text from the stdin
- * @list_words: list of strings that will contain the words of the buffer
+ * prompt_init - convert the input into a
+ * list of words and execute built-ins if needed
  *
- * Return: return 1 to continue the loop, 0 to exit the loop and -1 for an error
+ * @buffer: input to be converted
+ * @list_words: list that will contain the words
+ *
+ * Return: 2 to execute a command, 1 to just continue the loop,
+ * 0 to stop and -1 for an error
  */
-int prompt(const char *shell_name)
+int prompt_init(char *buffer, char **list_words)
 {
-	ssize_t res = 0;
-	size_t buf_size = 0;
-	char *buffer = NULL, *path = NULL;
-	pid_t child_pid = 0;
-	int status, atty = isatty(0);
-	char **list_words = NULL;
-
-	if(atty == 0)
-		errno = 0;
-	else
-		printf("#cisfun$ ");
-	res = getline(&buffer, &buf_size, stdin);
-	if (res == -1)
-	{
-		free(buffer);
-		if (errno != 0)
-			return(-1);
-		else if(atty)
-			printf("\n");
-		return(0);
-	}
 	list_words = malloc(BUF_SIZE * sizeof(char *));
 	if (list_words == NULL)
 	{
@@ -183,27 +52,45 @@ int prompt(const char *shell_name)
 	if (string_to_list(buffer, list_words) == -1)
 	{
 		free(buffer);
-		return(-1);
+		return (-1);
 	}
 	if (list_words[0] == NULL)
 	{
 		free(buffer);
 		free(list_words);
-		return(1);
+		return (1);
 	}
 	if (strcmp(list_words[0], "exit") == 0)
 	{
 		free(buffer);
 		free(list_words);
-		return(0);
+		return (0);
 	}
-	if(strcmp(list_words[0], "env") == 0)
+	if (strcmp(list_words[0], "env") == 0)
 	{
 		print_env();
 		free(buffer);
 		free(list_words);
-		return(1);
+		return (1);
 	}
+	return (2);
+}
+
+/**
+ * exec_prompt - finds the path to the command and execute it
+ *
+ * @buffer: string containing the input
+ * @list_words: list of pointers to each word of the input
+ *
+ * Return: return 1 to continue the loop,
+ * 0 to exit the loop and -1 for an error
+ */
+int exec_prompt(char *buffer, char **list_words)
+{
+	char *path = NULL;
+	int status;
+	pid_t child_pid = 0;
+
 	path = get_path(list_words[0]);
 	if (path == NULL)
 	{
@@ -213,19 +100,20 @@ int prompt(const char *shell_name)
 		free(list_words);
 		exit(127);
 	}
+
 	child_pid = fork();
 	if (child_pid == -1)
 	{
 		free(path);
 		free(buffer);
 		free(list_words);
-		return(-1);
+		return (-1);
 	}
 	if (child_pid == 0)
 	{
 		if (execve(path, list_words, NULL) == -1)
-			return(-1);
-		return(-1);
+			return (-1);
+		return (-1);
 	}
 	else
 	{
@@ -233,13 +121,48 @@ int prompt(const char *shell_name)
 		free(path);
 		free(buffer);
 		free(list_words);
-		return(1);
+		return (1);
 	}
 }
 
 /**
+ * prompt - waits for a command from stdin and execute it in a child process
+ *
+ * @shell_name: name of the executable
+ *
+ * Return: return 1 to continue the loop,
+ * 0 to exit the loop and -1 for an error
+ */
+int prompt(const char *shell_name)
+{
+	ssize_t res = 0;
+	size_t buf_size = 0;
+	char *buffer = NULL, *path = NULL, **list_words = NULL;
+	int atty = isatty(0);
+
+	if (atty == 0)
+		errno = 0;
+	else
+		printf("#cisfun$ ");
+	res = getline(&buffer, &buf_size, stdin);
+	if (res == -1)
+	{
+		free(buffer);
+		if (errno != 0)
+			exit(errno);
+		else if (atty)
+			printf("\n");
+		return (0);
+	}
+	ret = prompt_init();
+	if (ret != 0)
+		return (ret);
+	return (exec_prompt(buffer, list_words));
+}
+
+/**
  * main - program to run a simple shell
- * 
+ *
  * @ac: number of arguments given
  * @av: list of arguments given
  *
@@ -250,16 +173,17 @@ int main(int ac __attribute__((unused)), char **av)
 	int ret = 0;
 	const char *name = av[0];
 
-	while (1) {
+	while (1)
+	{
 		ret = prompt(name);
-		if(ret == -1)
+		if (ret == -1)
 			perror(name);
 		if (ret != 1)
 			break;
 	}
-	if(ret == -1)
+	if (ret == -1)
 		return (errno);
-	if(stderr == 0)
+	if (stderr == 0)
 		fprintf(stderr, "%s", "");
-	return(0);
+	return (0);
 }
